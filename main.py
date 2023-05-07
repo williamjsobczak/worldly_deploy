f7f413160a735b23e2ba24767316a6d8123f3193
WilliamSobczak <williamjsobczak@gmail.com>
2023-05-07 10:58:05 -0400

    wsgi config

--- main.py
+++ main.py
@@ -227,5 +227,9 @@ def update_continent_line_graph(metric):
 #     app.run()
 #     # app.run_server(debug=True)
 
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=8050)
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8050)
+    app.run_server(debug=True)
