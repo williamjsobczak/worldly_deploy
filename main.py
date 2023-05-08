f428b1e7ea407fed73969fd5d0dd7010bec7232c
WilliamSobczak <williamjsobczak@gmail.com>
2023-05-07 22:43:30 -0400

    ipv4

--- main.py
+++ main.py
@@ -230,7 +230,7 @@ def update_continent_line_graph(metric):
 
 #working local
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='129.213.16.92', port=8080)
 
 
 # if __name__ == '__main__':
